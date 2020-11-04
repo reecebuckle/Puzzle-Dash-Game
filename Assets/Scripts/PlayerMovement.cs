e17ff1f395a47e7b2724c28cf164eec26d955fd5
MihaiNuica97 <mihainuica@gmail.com>
2020-11-04 16:34:47 +0000

    very basic movement implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    public float speed = 12f;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-
+        controller = gameObject.GetComponent<CharacterController>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     controller.Move(transform.position);
+        // }
 
     }
 }
