dda13d3501e6b25a72c17d96cbb4d4b2872b1ee6
Reece Buckle <reecebuckle@icloud.com>
2020-11-19 18:46:07 +0000

    my git is fucked

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    float speed;
-
-    public float walkSpeed = 10f;
-
-    public float sprintSpeed = 20f;
-
+    public float speed = 10f;
     public float gravity = -10f;
     public float jumpHeight = 5f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     Vector3 velocity;
     bool isGrounded;
-    public float verticalVeloticy;
-
+    
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-
-        isGrounded = controller.isGrounded;
-        verticalVeloticy = controller.velocity.y;
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            speed = sprintSpeed;
-        }
-        else
-        {
-            speed = walkSpeed;
-        }
-
-        if (isGrounded && velocity.y < 0)
-        {
+        if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
-
-
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -48,12 +32,11 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        velocity.y += (gravity * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
