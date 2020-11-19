8f5a516be3ec23ec2211765dfff96450d1da6d70
MihaiNuica97 <mihainuica@gmail.com>
2020-11-19 18:23:06 +0000

    sprint implemented and small fixes

--- Assets/Scripts/DaniCameraMove.cs
+++ Assets/Scripts/DaniCameraMove.cs
@@ -5,7 +5,7 @@ public class DaniCameraMove : MonoBehaviour
 
     public Transform player;
 
-    void Update()
+    void FixedUpdate()
     {
         transform.position = player.transform.position;
     }
